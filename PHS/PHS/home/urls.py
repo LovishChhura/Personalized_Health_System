019d6b6b74f432acee12b6f2bdabba48a0ca72f7
Lovish <vishaledu786@gmail.com>
2023-11-24 14:07:39 +0530

    added lohin func

--- PHS/PHS/home/urls.py
+++ PHS/PHS/home/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path("",views.home,name='home'),
+    # path("",views.home,name='home'),
+    path("",views.index,name='index'),
     path("reduce_stress/",views.reduce_stress,name='reduce_stress'),
     path("login/",views.login_page,name='login_page'),
+    path("register/",views.register,name='register'),
+    path("logout/",views.logout_page,name='logout_page'),
 ]
--- PHS/PHS/home/views.py
+++ PHS/PHS/home/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate , login ,logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url="/login/")
 def index(request):
     return render(request,'user_index.html')
 
+@login_required(login_url="/login/")
 def reduce_stress(request):
     return render(request,'reduce_stress.html')
 
@@ -11,4 +17,46 @@ def home(request):
     return render(request,'home.html')
 
 def login_page(request):
+    if request.method=="POST":
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        
+        if not User.objects.filter(email=email).exists():
+            messages.error(request,'Invalid email-id')
+            return redirect('/login/')
+        user=authenticate(email=email,password=password)
+        if user is None:
+            messages.error(request,'Invalid credentials')
+            return redirect('/login/')
+        else:
+            login(request,user)
+            return redirect('/user/')
+            
+    return render(request,'login.html')
+
+def logout_page(request):
+    logout(request)
+    return redirect('/')
+
+def register(request):
+    if request.method=="POST":
+        first_name=request.POST.get('first_name')
+        last_name=request.POST.get('last_name')
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        
+        user=User.objects.filter(email=email)
+        if user.exists():
+            messages.info(request,'User already exist')
+            return redirect('/register/')
+        
+        user=User.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+        )
+        user.set_password(password)
+        user.save()
+        messages.info(request,'Registered Successfully')
+        return redirect('/login/')
     return render(request,'login.html')
